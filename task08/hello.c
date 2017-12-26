4529f7f8e615a9b1251ffcd082bdfd59efcebf20
Mohammed Gamal <m.gamal005@gmail.com>
2017-12-26 20:02:32 +0200

    Task 08: add foo file

--- task08/hello.c
+++ task08/hello.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/debugfs.h>
 #include <linux/uaccess.h>
+#include <linux/semaphore.h>
 #include <linux/fs.h>
 
 MODULE_LICENSE("GPL");
@@ -9,8 +10,11 @@ MODULE_LICENSE("GPL");
 #define ID_LENGTH 13
 
 static const char id[] = "88a35103c0cc";
+static char foo[PAGE_SIZE];
 
 static struct dentry *parent_dir;
+
+DEFINE_MUTEX(foo_mutex);
 
 static ssize_t id_read(struct file *filp,
                 char __user *buffer,
@@ -56,14 +60,60 @@ static ssize_t id_write(struct file *filp,
         return len;
 }
 
+static ssize_t foo_read(struct file *filp,
+		char __user *buffer,
+		size_t len,
+		loff_t *offset)
+{
+	ssize_t ret;
+
+	mutex_lock(&foo_mutex);
+
+	ret = simple_read_from_buffer(buffer, len, offset, foo, PAGE_SIZE);
+
+	mutex_unlock(&foo_mutex);
+
+	return ret;
+}
+
+static ssize_t foo_write(struct file *filp,
+		const char __user *buffer,
+		size_t len,
+		loff_t *offset)
+{
+	ssize_t ret;
+
+	if (len > PAGE_SIZE) {  
+		pr_err("hello: Can't write more than %lu bytes\n", PAGE_SIZE);
+		return -EINVAL;
+	}
+
+	mutex_lock(&foo_mutex);
+
+	memset(foo, 0, sizeof(foo));
+	ret = simple_write_to_buffer(foo, PAGE_SIZE, offset, buffer, len);
+
+	mutex_unlock(&foo_mutex);
+
+	return ret;
+}
+
 static struct file_operations id_ops = {
 	.owner = THIS_MODULE,
 	.read = id_read,
 	.write = id_write
 };
+
+static struct file_operations foo_ops = {
+	.owner = THIS_MODULE,
+	.read = foo_read,
+	.write = foo_write
+};
+
 static int hello_init(void)
 {
 	struct dentry* id_entry;
+	struct dentry* foo_entry;
 
 	pr_debug("Hello World!\n");
 
@@ -82,12 +132,19 @@ static int hello_init(void)
 	id_entry = debugfs_create_file("id", 0644, parent_dir, NULL, &id_ops);
 
 	if (!id_entry) {
-		pr_err("Failed to create file 'id' \n");
+		pr_err("Failed to create file 'id'\n");
 		return -1;
 	}
 
 	if (!debugfs_create_u64("jiffies", 0444, parent_dir, (u64 *)&jiffies))
 		return -1;
+
+	foo_entry = debugfs_create_file("foo", 0666, parent_dir, NULL, &foo_ops);
+
+	if (!foo_entry) {
+		pr_err("Failed to create file 'foo'\n");
+		return -1;
+	}
 
 	return 0;
 }
@@ -100,4 +157,3 @@ static void hello_exit(void)
 
 module_init(hello_init);
 module_exit(hello_exit);
-
